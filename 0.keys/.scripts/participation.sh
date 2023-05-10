743c3a6e68cd02015203d0caa78ac4ea708f7cc5
setrar <setrar@valiha.com>
2023-05-09 22:24:32 -0400

    .

--- 0.keys/.scripts/participation.sh
+++ 0.keys/.scripts/participation.sh
@@ -37,7 +37,7 @@ i=0
 
 for id in "${ETUDIANTS[@]}"
 do
-   URL=[${IDS[${i}]}](https://github.com/${IDS[${i}]})
+   URL=[${IDS[${i}]}]\(https://github.com/${IDS[${i}]}\)
    FILE=${id}/README.md
 #   OK="| ${i} || [${id}](../${FILE}) - [${IDS[${i}]}]()<image src='https://avatars0.githubusercontent.com/u/${AVATARS[$i]}?s=460&v=4' width=20 height=20></image> | :heavy_check_mark: |"
 #   KO="| ${i} | [${id}](../${FILE}) - [${IDS[${i}]}]<image src='https://avatars0.githubusercontent.com/u/${AVATARS[$i]}?s=460&v=4' width=20 height=20></image> | :x: |"
